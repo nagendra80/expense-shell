cb8e1d80d238cccd6eb281d2976f8f0c6b689d27
Nagendra Kadumuru <nagendrakadumuru83@gmail.com>
2025-04-22 23:51:38 +0530

    adding one space in mysql server script

--- mysql.sh
+++ mysql.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Starting MySQL Server"
 
 mysql -h mysql.pa1.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
 
     echo "MySQL Root Passsword not setup" &>>$LOG_FILE_NAME
